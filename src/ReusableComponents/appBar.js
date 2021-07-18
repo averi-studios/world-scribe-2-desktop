ac27b1f969b7be5d7c9d2177cbbf40f754d357a8
Michele Moio <michele.moio.4d@gmail.com>
2021-07-18 09:33:20 +0200

    editable appBar title

--- src/ReusableComponents/appBar.js
+++ src/ReusableComponents/appBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,6 +9,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import BackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from '@material-ui/core/IconButton';
 import classNames from 'classnames';
+import { InputBase } from '@material-ui/core';
 
 const styles = {
     root: {
@@ -41,37 +42,73 @@ const styles = {
 };
 
 class ButtonAppBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTitleEditable: false,
+            newTitle: '',
+        };
+    }
+
+    toggleEditableTitle = () => {
+        this.setState({
+            isTitleEditable: !this.state.isTitleEditable
+        });
+    }
+
+    handleTitleChange = () => {
+        this.props.handleTitleChange(this.state.newTitle);
+        this.setState({
+            isTitleEditable: false,
+            newTitle: ''
+        })
+    }
+
     render(){
-    const { classes,open,history } = this.props;
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        aria-label="Open drawer"
-                        onClick={this.props.handleDrawerOpen}
-                        className={classNames(classes.menuButton, open)}
-                    >
-                        <MenuIcon
-                            className={classes.menuButtonIcon}
-                        />
-                    </IconButton>
-                    <div className={classes.grow}>
+        const { classes,open,history } = this.props;
+        const { isTitleEditable } = this.state;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
                         <IconButton
-                            aria-label="Go back one page"
-                            onClick={history.goBack}
-                            className={classes.backButton}
+                            aria-label="Open drawer"
+                            onClick={this.props.handleDrawerOpen}
+                            className={classNames(classes.menuButton, open)}
                         >
-                            <BackIcon className={classes.backButtonIcon} />
+                            <MenuIcon
+                                className={classes.menuButtonIcon}
+                            />
                         </IconButton>
-                        <Typography variant="h6" color="inherit">
-                            {this.props.title}
-                        </Typography>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+                        <div className={classes.grow}>
+                            <IconButton
+                                aria-label="Go back one page"
+                                onClick={history.goBack}
+                                className={classes.backButton}
+                            >
+                                <BackIcon className={classes.backButtonIcon} />
+                            </IconButton>
+                            {isTitleEditable && this.props.editable ? (
+                                <form onSubmit={this.handleTitleChange} >
+                                    <InputBase
+                                        autoFocus
+                                        type="input"
+                                        placeholder={this.props.title}
+                                        value={this.state.newTitle}
+                                        onChange={(e) => this.setState({ newTitle: e.target.value })}
+                                        onBlur={this.handleTitleChange}
+                                    />
+                                </form>
+                            ) : (
+                                <Typography variant="h6" color="inherit" onClick={this.toggleEditableTitle}>
+                                    {this.props.title}
+                                </Typography>
+                            )}
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
     }
 }
 
